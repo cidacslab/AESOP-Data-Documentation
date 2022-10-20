c1113e2545bbd317591b7de235059d1d7ac60df6
Julian-sun <45834356+Julian-sun@users.noreply.github.com>
2022-10-20 16:40:50 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,10 @@ author = 'Juliane'
 release = '0.1'
 version = '0.1.0'
 
+import sys
+import os
+import shlex
+
 # -- General configuration
 
 extensions = [
@@ -27,9 +31,12 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
+htmlhelp_basename = 'TableswithSphinxdoc'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
